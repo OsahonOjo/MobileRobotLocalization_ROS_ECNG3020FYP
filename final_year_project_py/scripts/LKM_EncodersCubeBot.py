825210bfeb2170a338a884381af9ca68d38119e0
OsahonOjo <osahonobor@yahoo.com>
2023-08-02 01:36:21 -0600

    modified innovation-related code to only account for bearing data

--- final_year_project_py/scripts/LKM_EncodersCubeBot.py
+++ final_year_project_py/scripts/LKM_EncodersCubeBot.py
@@ -28,7 +28,7 @@ KL = 0.00375                 		# error constant: left wheel
 BEACON_DETECTOR_RESOLUTION = 5*(math.pi/180)	# radians
 BEACON_DETECTOR_STD_DEVIATION = 0.34 * BEACON_DETECTOR_RESOLUTION
 BEACON_DETECTOR_VARIANCE = BEACON_DETECTOR_STD_DEVIATION**2
-beaconDataCovariance = numpy.diag([0, BEACON_DETECTOR_VARIANCE])
+beaconDataCovariance = numpy.array([BEACON_DETECTOR_VARIANCE]).reshape((1,1))
 bearingNoiseSamples = numpy.random.default_rng().normal(0, BEACON_DETECTOR_VARIANCE, 1000)
 bearingNoiseSamplesIndex = 0
 
@@ -39,8 +39,8 @@ with open('/home/osahon/catkin_ws/src/final_year_project_py/scripts/initialValue
     initialPose2D = data['EncodersCubeBotInitialPose']
     beaconPositions = { "beacon1": data['Beacon1Position'], "beacon2": data['Beacon2Position'] }
 
-state = numpy.array([[initialPose2D['x']], [initialPose2D['y']], [initialPose2D['theta']]])
-p = numpy.diag([0, 0, 0])
+state = numpy.array([[initialPose2D['x']], [initialPose2D['y']], [initialPose2D['theta']]]).reshape((3,1))
+p = numpy.diag([0, 0, 0]).reshape((3,3))
 uncertainty = 0
 
 posePublisher = rospy.Publisher('/EncodersCubeBot/pose_estimate', PoseWithCovarianceStamped, queue_size=10)  
@@ -71,18 +71,20 @@ def LKMPredictionStep(odometry):
 	thetaArg = theta + (delTheta/2)	
 
 	state = state + [[delS*math.cos(thetaArg)], [delS*math.sin(thetaArg)], [delTheta]]
-	odometryCovariance = numpy.diag([errorSr, errorSl])	
+	odometryCovariance = numpy.diag([errorSr, errorSl]).reshape((2,2))
 
 
 	# 2. predict covariance
 
 	# partial derivative of configuration w.r.t state (x,y,z)
-	fp = numpy.array([[1, 0, -1*delS*math.sin(thetaArg)], [0, 1, delS*math.cos(thetaArg)], [0, 0, 1]])
-	fpT = fp.transpose()
+	fp = numpy.array([[1, 0, -1*delS*math.sin(thetaArg)], [0, 1, delS*math.cos(thetaArg)], [0, 0, 1]]).reshape((3,3))
+	fpT = fp.transpose().reshape((3,3))
+	# print("fpT: ", numpy.shape(fpT))  # fpT:  (3, 3)
 
 	# partial derivative of configuration w.r.t odometry (del_sl, del_sr)
-	fs = numpy.array([[(0.5*math.cos(thetaArg))-(delS/(2*WHEELBASE))*math.sin(thetaArg), (0.5*math.cos(thetaArg))+((delS/(2*WHEELBASE))*math.sin(thetaArg))], [(0.5*math.sin(thetaArg))+(delS/(2*WHEELBASE))*math.cos(thetaArg), (0.5*math.sin(thetaArg))-((delS/(2*WHEELBASE))*math.cos(thetaArg))], [(1/WHEELBASE), (-1/WHEELBASE)]])
-	fsT = fs.transpose()
+	fs = numpy.array([[(0.5*math.cos(thetaArg))-(delS/(2*WHEELBASE))*math.sin(thetaArg), (0.5*math.cos(thetaArg))+((delS/(2*WHEELBASE))*math.sin(thetaArg))], [(0.5*math.sin(thetaArg))+(delS/(2*WHEELBASE))*math.cos(thetaArg), (0.5*math.sin(thetaArg))-((delS/(2*WHEELBASE))*math.cos(thetaArg))], [(1/WHEELBASE), (-1/WHEELBASE)]]).reshape((3,2))
+	fsT = fs.transpose().reshape((2,3))
+	# print("fsT: ", numpy.shape(fsT))  # fsT:  (2, 3)
 	
 	p = (fp @ p @ fpT) + (fs @ odometryCovariance @ fsT)
 
@@ -118,41 +120,155 @@ def LKMUpdateStepWithBeaconData(beaconData):
 	robotTheta = state[2][0]
 	expectedBeaconRange = math.sqrt((beaconX - robotX)**2 + (beaconY - robotY)**2)
 
-	measuredBeaconData = numpy.array([[expectedBeaconRange], [measuredBearing]])	# radians
-	expectedBeaconData = numpy.array([[expectedBeaconRange], [math.atan2((beaconY - robotY), (beaconX - robotX)) - robotTheta]])
+	measuredBeaconData = numpy.array([measuredBearing]).reshape((1,1))	# radians
+	expectedBeaconData = numpy.array([math.atan2((beaconY - robotY), (beaconX - robotX)) - robotTheta]).reshape((1,1))
 	
-	beaconDataNoise = numpy.array([[0], [bearingNoiseSamples[bearingNoiseSamplesIndex]]])
+	beaconDataNoise = numpy.array([bearingNoiseSamples[bearingNoiseSamplesIndex]]).reshape((1,1))
 	bearingNoiseSamplesIndex = 0 if bearingNoiseSamplesIndex == len(bearingNoiseSamples)-1 else bearingNoiseSamplesIndex + 1
 	innovation = measuredBeaconData - expectedBeaconData + beaconDataNoise
 
 
 	# 4. update state estimate
 
-	hw = numpy.array([[1, 0], [0, 1]])
-	hwT = hw.transpose()
+	# hw = numpy.array([[1, 0], [0, 1]])
+	print("p: \n", p)
+	print("beaconDataCovariance: \n", beaconDataCovariance)
+
 	r = expectedBeaconRange
-	hx = numpy.array([[-(beaconX - robotX)/r, -(beaconY - robotY)/r, 0], [(beaconY - robotY)/r**2, -(beaconX - robotX)/r**2, -1]])
-	hxT = hx.transpose()
-	s = hx @ p @ hxT + hw @ beaconDataCovariance @ hwT
-	try:
-		sInverse = numpy.linalg.inv(s)	# A non-square matrix does not have an inverse
-	except numpy.linalg.LinAlgError("Singular matrix") as e:
-		print(e)	# A square matrix is singular if and only if its determinant is 0
-	k = p @ hxT @ sInverse
-	state = state + (k @ innovation)
+	print("r: \n", r)
 
-	// only_bearing_innovation_no_range branch
+	hw = numpy.array([1]).reshape((1,1))  # hw = dh_bearing/dw_bearing => 1x1 scalar
+	print("hw: \n", hw)
+	hwT = numpy.transpose(hw).reshape((1,1))
+	print("hwT: ", numpy.shape(hwT))
 
+	# hx = numpy.array([[-(beaconX - robotX)/r, -(beaconY - robotY)/r, 0], [(beaconY - robotY)/r**2, -(beaconX - robotX)/r**2, -1]])
+	hx = numpy.array([(beaconY - robotY)/r**2, -(beaconX - robotX)/r**2, -1]).reshape((1,3))  # 1x3 vector
+	print("hx: \n", hx)
+	hxT = numpy.transpose(hx).reshape((3,1))
+	print("hxT: ", numpy.shape(hxT))
+	
+	# LESSON: 
+	# when multiplying with 1x1 scalar, use * (element-wise mult.) instead of @ (matrix mult.)
+	# you'll still get the result you expect
+	# ALWAYS use reshape() to explicitly set matrix dimensions
+
+	# s1 = hx @ p @ hxT  # 1x1 scalar
+	# print("s1: \n", s1)
+	# s2 = hw * beaconDataCovariance * hwT  # changed @ to *
+	# 1x1 scalar
+	# operator * which was used either for element-wise multiplication or matrix multiplication depending on the convention employed in that particular library/code. As a result, in the future, the operator * is meant to be used for element-wise multiplication only
+	# print("s2: \n", s2)
+	s = (hx @ p @ hxT) + (hw * beaconDataCovariance * hwT)  # 1x1 scalar
+	print("s: \n", s)
+
+	# s = hx @ p @ hxT + hw @ beaconDataCovariance @ hwT  # 1x1 scalar
+	# print("s: \n", s)
+	# inverse of a 1x1 matrix is simply the reciprical of the single entry in the matrix
+	# try:
+	# 	sInverse = numpy.linalg.inv(s)	# A non-square matrix does not have an inverse
+	# except numpy.linalg.LinAlgError("Singular matrix") as e:
+	# 	print(e)	# A square matrix is singular if and only if its determinant is 0
+	sInverse = 1/s  # 1x1 scalar
+	print("sInverse: \n", sInverse)
+	
+	# kscalar = hxT * sInverse
+	# kscalar = numpy.reshape(kscalar, (3,1))
+	# print("kscalar: ", numpy.shape(kscalar))
+	# print(kscalar)
+
+	k = p @ (hxT * sInverse)  # 3x3 3x1 1x1 => 3x3 3x1 => 3x1
+	k = numpy.reshape(k, (3,1))
+	print("k: ", numpy.shape(k))
+	print(k)
+	
+	state = state + (k * innovation)  # 3x1 + (3x1 1x1) => 3x1
+	print("state: \n", state)
 
 	# 5. update covariance
+	# psub = k @ (hx @ p)
+	# psub = numpy.reshape(psub, (3,3))
+	# print("psub: \n", psub)
 
 	p = p - (k @ hx @ p)
+	print("p: \n", p)
+
 	robotPoseDeterminant = ( p[0][0] * ( (p[1][1]*p[2][2]) - (p[2][1]*p[1][2]) )) - ( p[0][1] * ( (p[1][0]*p[2][2]) - (p[2][0]*p[1][2]) )) + ( p[0][2] * ( (p[1][0]*p[2][1]) - (p[1][1]*p[2][0]) ))
 	uncertainty = math.sqrt(abs(robotPoseDeterminant))
 
-	# print("state: \n", state)
-	# print("Covariance matrix: \n", p)
-	# print("uncertainty: \n", uncertainty)
+	# From Prediction Step
+	# p: 
+	# [[1.16729523e-06 3.13611861e-06 6.68494671e-05]
+	# [3.13611861e-06 9.40076721e-06 1.98167016e-04]
+	# [6.68494671e-05 1.98167016e-04 4.18230873e-03]]
+	# beaconDataCovariance: 
+	# [[0.00088034]]
+	# r: 
+	# 1.033969272961315
+	# hw: 
+	# [[1]]
+	# hwT:  (1, 1)
+	# hx: 
+	# [[ 0.20245612 -0.94571894 -1.        ]]
+	# hxT:  (3, 1)
+	# s: 
+	# [[0.00541766]]
+	# sInverse: 
+	# [[184.58152407]]
+	# k:  (3, 1)
+	# [[-0.012843  ]
+	# [-0.03810179]
+	# [-0.80407126]]
+	# state: 
+	# [[0.22252566]
+	# [0.40676142]
+	# [0.41427224]]
+	# p: 
+	# [[2.73691566e-07 4.85032883e-07 1.09029708e-05]
+	# [4.85032883e-07 1.53569701e-06 3.21885582e-05]
+	# [1.09029708e-05 3.21885582e-05 6.79625626e-04]]
+	# From Update Step
+	# p: 
+	# [[2.73691566e-07 4.85032883e-07 1.09029708e-05]
+	# [4.85032883e-07 1.53569701e-06 3.21885582e-05]
+	# [1.09029708e-05 3.21885582e-05 6.79625626e-04]]
+	# beaconDataCovariance: 
+	# [[0.00088034]]
+	# r: 
+	# 0.3205065356189586
+	# hw: 
+	# [[1]]
+	# hwT:  (1, 1)
+	# hx: 
+	# [[ 2.4944312   1.87419176 -1.        ]]
+	# hxT:  (3, 1)
+	# s: 
+	# [[0.00139655]]
+	# sInverse: 
+	# [[716.04830459]]
+	# k:  (3, 1)
+	# [[-0.00666728]
+	# [-0.02012131]
+	# [-0.42397319]]
+	# state: 
+	# [[0.23454169]
+	# [0.4430248 ]
+	# [1.17837265]]
+	# p: 
+	# [[2.11611007e-07 2.97678952e-07 6.95526253e-06]
+	# [2.97678952e-07 9.70278530e-07 2.02747053e-05]
+	# [6.95526253e-06 2.02747053e-05 4.28590511e-04]]
+	# From Update Step
+
+
+
+	# [ERROR] [1690955382.326007]: bad callback: <function LKMUpdateStepWithBeaconData at 0x7fae8f606160>
+	# Traceback (most recent call last):
+	# File "/opt/ros/noetic/lib/python3/dist-packages/rospy/topics.py", line 750, in _invoke_callback
+	# 	cb(msg)
+	# File "/home/osahon/catkin_ws/src/final_year_project_py/scripts/LKM_EncodersCubeBot.py", line 150, in LKMUpdateStepWithBeaconData
+	# 	s2 = hw @ beaconDataCovariance @ hwT
+	# ValueError: matmul: Input operand 0 does not have enough dimensions (has 0, gufunc core with signature (n?,k),(k,m?)->(n?,m?) requires 1)
 
 	print("From Update Step")
 	publishPoseWithCovariance(state, p)
@@ -176,7 +292,7 @@ def eulerToQuaternion(roll, pitch, yaw):
 
 
 def publishPoseWithCovariance(pose2D, covariance):
-	pose = {}
+	pose = {"position": {}, "orientation": {}}
 	pose['position'] = { "x": round(pose2D[0][0], 3), "y": round(pose2D[1][0], 3), "z": 0 }
 	qx, qy, qz, qw = eulerToQuaternion(0, 0, round(pose2D[2][0]))
 	pose['orientation'] = { "x": qx, "y": qy, "z": qz, "w": qw }
